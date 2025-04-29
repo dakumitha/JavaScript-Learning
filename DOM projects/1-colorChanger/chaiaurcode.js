7ba9c91ac94532bafe8933857aa38ae850e60624
dakumitha <dakumitha@gmail.com>
2025-04-30 01:14:27 +0530

    changes made from vscode

--- DOM projects/1-colorChanger/chaiaurcode.js
+++ DOM projects/1-colorChanger/chaiaurcode.js
@@ -1,7 +1,25 @@
-const greyItem = document.getElementById("grey");
-const yellowItem = document.getElementById("yellow");
-const whiteItem = document.getElementById("white");
+// const greyItem = document.getElementById("grey");
+// const yellowItem = document.getElementById("yellow");
+// const whiteItem = document.getElementById("white");
+// const blueItem = document.getElementById("blue");
 
-greyItem.style.backgroundColor = "green";
-yellowItem.style.backgroundColor = "red";
-whiteItem.style.backgroundColor = "darkgrey";
+// greyItem.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "grey";
+// });
+// yellowItem.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "yellow";
+// });
+// whiteItem.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "white";
+// });
+// blueItem.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "blue";
+// });
+
+const colorSwitcher = document.querySelectorAll(".button");
+colorSwitcher.forEach((button) => {
+  button.addEventListener("click", () => {
+    document.body.style.backgroundColor = button.id;
+    console.log(button.id);
+  });
+});
