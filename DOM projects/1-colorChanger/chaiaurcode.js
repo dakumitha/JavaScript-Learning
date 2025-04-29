c2258b3c7aba1ba6d90b1036a7406a997642a81a
dakumitha <dakumitha@gmail.com>
2025-04-30 00:03:54 +0530

    changes made locally

--- DOM projects/1-colorChanger/chaiaurcode.js
+++ DOM projects/1-colorChanger/chaiaurcode.js
@@ -1,5 +1,7 @@
 const greyItem = document.getElementById("grey");
 const yellowItem = document.getElementById("yellow");
+const whiteItem = document.getElementById("white");
 
 greyItem.style.backgroundColor = "green";
 yellowItem.style.backgroundColor = "red";
+whiteItem.style.backgroundColor = "darkgrey";
