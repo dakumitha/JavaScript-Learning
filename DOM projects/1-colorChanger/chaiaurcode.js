15760fbcbc0c5fa04b14b96723e17774f4576332
dakumitha <dakumitha@gmail.com>
2025-04-29 18:28:11 +0000

    changes from codespaces

--- DOM projects/1-colorChanger/chaiaurcode.js
+++ DOM projects/1-colorChanger/chaiaurcode.js
@@ -1,3 +1,5 @@
 const greyItem = document.getElementById("grey");
+const yellowItem = document.getElementById("yellow");
 
 greyItem.style.backgroundColor = "green";
+yellowItem.style.backgroundColor = "red";
