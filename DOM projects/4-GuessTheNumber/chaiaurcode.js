816924791bbcc59efc3248fb6a47bc420404899f
dakumitha <dakumitha@gmail.com>
2025-05-01 23:14:19 +0530

    changes made locally

--- DOM projects/4-GuessTheNumber/chaiaurcode.js
+++ DOM projects/4-GuessTheNumber/chaiaurcode.js
@@ -1,2 +1,23 @@
 const randomNumber = parseInt(Math.random() * 100) + 1;
 console.log(randomNumber);
+const inputField = document.getElementById("guessField");
+const submit = document.getElementById("subt");
+const prevGuesses = document.querySelector(".guesses");
+const guessesRemaining = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHi");
+
+submit.addEventListener("click", function () {
+  const userGuess = parseInt(inputField.value);
+  const p = document.createElement("p");
+
+  if (userGuess === randomNumber) {
+    p.textContent = "You guessed it right!";
+    prevGuesses.insertAdjacentElement("beforebegin", p);
+  } else if (userGuess < randomNumber) {
+    p.textContent = "Your guess is too low!";
+    prevGuesses.insertAdjacentElement("beforebegin", p);
+  } else {
+    p.textContent = "Your guess is too high!";
+    prevGuesses.insertAdjacentElement("beforebegin", p);
+  }
+});
